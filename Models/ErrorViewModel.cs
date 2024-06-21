e704faa00ca9fb01401ccec394830078ed4e2591
furkan aydin <furknn2112@gmail.com>
2024-06-21 22:38:59 +0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GlobalExceptionHandling.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
